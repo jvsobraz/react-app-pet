5636cbaa8073011d0849270f3ab1ca03cce94876
ViniGarciaa <rm95697@fiap.com.br>
2022-09-29 23:17:58 -0300

    criando o app.jsx

---
+++ react-app/react-pet/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from react
+
+export default function App() {
+
+    return(
+        <>
+            <h1>Dados do Pet</h1>
+            <PetCadastro/>
+        
+        
+        </>
+    )
+    
+}
