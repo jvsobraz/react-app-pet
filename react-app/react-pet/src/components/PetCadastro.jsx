3981f3da0d2016d77532c2e94bfe7ad9123be3ab
ViniGarciaa <rm95697@fiap.com.br>
2022-09-29 23:32:23 -0300

    criando os componentes 1

---
+++ react-app/react-pet/src/components/PetCadastro.jsx
@@ -0,0 +1,16 @@
+import React from React
+
+export default function PetCadastro(props){
+    <>
+        <div>Informações</div>
+        <h2>Nome:</h2>
+        <h2>Idade:</h2>
+        <h2>Raça:</h2>
+        <h2>Tamanho:</h2>
+        <h2>Nome do Dono:</h2>
+        <h2>Telefone do Dono:</h2>
+        <h2>Imagem do Pet:</h2>
+        <h2>Obserfações:</h2>
+    
+    </>
+}
