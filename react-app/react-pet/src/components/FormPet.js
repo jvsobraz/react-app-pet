e4a28e9cdefcb224bcad2e9c8dc88f9e163272f7
rm95512 <rm95512@fiap.com.br>
2022-10-01 13:34:27 -0300

    Modificações e inclusões dentro do formulário

--- react-app/react-pet/src/components/FormPet.js
+++ react-app/react-pet/src/components/FormPet.js
@@ -2,15 +2,15 @@ function PetForm() {
     return (
         
         <form>
-             <input type="text" placeholder="Insira o nome do pet"/>
-             <input type="number" placeholder="Insira a idade do pet"/> 
-             <input type="text" placeholder="Insira a raça do pet"/>
-             <input type="number" placeholder="Insira o tamanho do pet"/> 
-             <input type="text" placeholder="Insira o nome do dono"/>
-             <input type="tel" placeholder="Insira o telefone para contato"/>
-             <input type="file" placeholder="Insira a imagem do pet"/> 
-             <input type = "text" placeholder="Insira uma observação"/>
-             <input type="submit" value= "Cadastrar" />     
+             <input class="nome" type="text" placeholder="Insira o nome do pet"/>
+             <input class="idade" type="number" placeholder="Insira a idade do pet"/> 
+             <input class="raca" type="text" placeholder="Insira a raça do pet"/>
+             <input class="tamanho" type="number" placeholder="Insira o tamanho do pet"/> 
+             <input class="dono" type="text" placeholder="Insira o nome do dono"/>
+             <input class="telefone" type="tel" placeholder="Insira o telefone para contato"/>
+             <input class="imagem" type="file" placeholder="Insira a imagem do pet"/> 
+             <input class="obs" type = "text" placeholder="Insira uma observação"/>
+             <input class="button" type="submit" value= "Cadastrar" />     
     
         </form>
     )
